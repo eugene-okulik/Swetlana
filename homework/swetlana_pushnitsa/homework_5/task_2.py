f9e8e50fd64a832f2dd5a01926fea417ca81804f
Swetlana <0720246s@gmail.com>
2024-12-19 15:01:46 +0100

    swetlana_pushnitsa_homework_5_done

---
+++ homework/swetlana_pushnitsa/homework_5/task_2.py
@@ -0,0 +1,14 @@
+string_1 = "Operation result: 42"
+string_2 = "Operation result: 514"
+string_3 = "Performance result: 9"
+
+string_1_index = string_1.index("42")
+string_1_result = int(string_1[string_1_index:]) + 10
+
+string_2_index = string_2.index("514")
+string_2_result = int(string_2[string_2_index:]) + 10
+
+string_3_index = string_3.index("9")
+string_3_result = int(string_3[string_3_index:]) + 10
+
+print(string_1_result, string_2_result, string_3_result)
