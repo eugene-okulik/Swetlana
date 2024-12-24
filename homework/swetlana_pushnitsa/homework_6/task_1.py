d715bbd3b73e7170bdcdd7480bef53871d6831ec
Swetlana <0720246s@gmail.com>
2024-12-24 11:54:09 +0100

    swetlana_pushnitsa_homework_6_done

---
+++ homework/swetlana_pushnitsa/homework_6/task_1.py
@@ -0,0 +1,15 @@
+text = "Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, dignissim vitae libero"
+words = text.split()
+
+new_text = []
+for word in words:
+    if word.endswith(","):
+        word = word.strip(",")
+        new_text.append(word + "ing,")
+    elif word.endswith("."):
+        word = word.strip(".")
+        new_text.append(word + "ing.")
+    else:
+        word = word + "ing"
+        new_text.append(word)
+print(" ".join(new_text))
