a1ddeffb14ceb7109b2ac1f0433b1b994bbb3595
Swetlana <0720246s@gmail.com>
2025-02-26 13:37:41 +0100

    swetlana_pushnitsa_homework_20 edit fstring

--- homework/swetlana_pushnitsa/homework_20/test_delete_object.py
+++ homework/swetlana_pushnitsa/homework_20/test_delete_object.py
@@ -11,7 +11,7 @@ def test_delete_object(create_obj, before_after):
         response_delete = requests.delete(f'http://167.172.172.115:52353/object/{create_obj}')
     with allure.step('Return the status code'):
         assert response_delete.status_code == 200
-    with allure.step(f'Get the post with deleting id'):
+    with allure.step(f'Get the post with deleting id {create_obj}'):
         response_get_after = requests.get(f'http://167.172.172.115:52353/object/{create_obj}')
     with allure.step('Return status code, to show that object is not found'):
         assert response_get_after.status_code == 404
